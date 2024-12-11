e1bc06d4e486e1ce5e5af7c4d7e14ca9f7f05fce
ColinFransman <colinfransman1@gmail.com>
2024-12-11 15:32:06 +0100

    fix: fixed 'unknown column password'

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable implements LaratrustUser
     protected $fillable = [
         'name',
         'email',
-        'password',
+        // 'password',
     ];
 
     /**
@@ -31,7 +31,7 @@ class User extends Authenticatable implements LaratrustUser
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        // 'password',
         'remember_token',
     ];
 
@@ -44,7 +44,7 @@ class User extends Authenticatable implements LaratrustUser
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            // 'password' => 'hashed',
         ];
     }
 }
