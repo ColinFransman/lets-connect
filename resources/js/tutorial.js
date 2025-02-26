5b777fd93ccf63846808b10f52f26d2c08461968
Jesper321 <drentj7@gmail.com>
2025-02-26 17:08:05 +0100

    Created styling for four steps, coloring icons for extra steps and busy with fourth step. added two more step texts.

--- resources/js/tutorial.js
+++ resources/js/tutorial.js
@@ -1,18 +1,21 @@
 let currentStepIndex = 0;
 // for buttons styling.
 let prevButton = document.getElementById('prevButton');
+let nextButton = document.getElementById('nextButton');
 let tutButtons = document.querySelector('.tutorial-buttons');
 
 // overlay
 let tutOverlay = document.querySelector('.tutorial-overlay');
 
 // first round
-let roundOne = document.querySelector('#round1 .round')
+let roundOne = document.querySelector('#round1 .round:nth-child(2)');
 
 const tutorialSteps = [
     { text: "Welkom op het dashboard! Laten we leren hoe je deze pagina gebruikt.", highlight: ".round:nth-child(1)" },
+    { text: "Klik op het 'i' tje om meer info te krijgen over een workshop zelf.", highlight: ".round:nth-child(1)" },
     { text: "Dit is Ronde 1. Sleep een workshop hierheen om het toe te wijzen aan deze ronde.", highlight: ".round:nth-child(1)" },
     { text: "Hier is een workshop. Klik en sleep het naar een ronde.", highlight: ".workshop:nth-child(1)" },
+    { text: "Je kunt ook de workshop weer verwijderen met de 'x'.", highlight: ".round:nth-child(1)" },
     { text: "Goed gedaan! Laat de workshop vallen in een ronde om je planning te voltooien.", highlight: ".round:nth-child(1)" }
 ];
 let tutorialLength = tutorialSteps.length - 1;
@@ -24,8 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function startTutorial() {
     const overlay = document.getElementById("tutorial-overlay"); // if ID exists, the popup becomes visible
     overlay.style.display = "flex";
-    document.getElementById('tutorial-text').textContent = tutorialSteps[currentStepIndex].text
-    console.log(currentStepIndex)
+    document.getElementById('tutorial-text').textContent = tutorialSteps[currentStepIndex].text;
     
     // styling buttons onload.
     if (currentStepIndex === 0) {
@@ -47,9 +49,17 @@ function nextStep() {
 
     // on tutorial round 1.
     if (currentStepIndex === 1) {
-        secondStep()
+        firstStep();
+        // nextButton.disabled = true; // disabling button.
+    } else if (currentStepIndex === 2) {
+        secondStep();
+    } else if (currentStepIndex === 3) {
+        thirdStep();
+    } else if (currentStepIndex === 4) {
+        fourthStep();
     } else {
-        defaultStyling()
+        defaultStyling();
+        // nextButton.disabled = false; // enabling button on other steps.
     }
 }
 
@@ -58,22 +68,31 @@ function prevStep() {
     document.getElementById('tutorial-text').textContent = tutorialSteps[currentStepIndex].text;
 
     // styling buttons
-    if (currentStepIndex === 0) { // styling on the first tutorial step and the other steps. 
+    if (currentStepIndex === 0) { // styling on the first tutorial step.
         prevButton.style.display = "none";
         tutButtons.style.justifyContent = "center";
-    } else {
+    } else { // styling other steps.
         prevButton.style.display = "flex";
         tutButtons.style.justifyContent = "space-between";
     }
 
     // on tutorial round 1.
     if (currentStepIndex === 1) {
-        secondStep()
+        firstStep();
+        // nextButton.disabled = true;
+    } else if (currentStepIndex === 2) {
+        secondStep();
+    } else if (currentStepIndex === 3) {
+        thirdStep();
+    } else if (currentStepIndex === 4) {
+        fourthStep();
     } else {
-        defaultStyling()
+        defaultStyling();
+        // nextButton.disabled = false;
     }
 }
 
+// returning steo indexes
 function hidePreviousOverlay() {
     // if index is lower than array length and is '0', the number increases.
     if(currentStepIndex < tutorialLength || currentStepIndex === 0) {
@@ -91,29 +110,89 @@ function showNextOverlay() {
     return currentStepIndex;
 }
 
-async function sendWorkshop() {
+function sendWorkshop() {
     // first workshop
     let workshopOne = document.getElementById('workshop0')
-    console.log(workshopOne)
-
     return workshopOne;
 }
 
-function defaultStyling() {
-    workshopOne = sendWorkshop();
-    // rounds
-    tutOverlay.style.alignItems = "center";
-    roundOne.style.zIndex = "1  ";
-
-    workshopOne.style.zIndex = "1";
+function sendInfoIcon() {
+    // first icon
+    let iconOne = document.getElementById('info0');
+    return iconOne;
 }
 
+function sendRoundX() {
+    let roundOneX;
+    if (currentStepIndex === 3) {
+        roundOneX = document.querySelector('#round1 .round .workshop .close-button')
+    }
+    console.log(roundOneX)
+    return roundOneX;
+}
+
+function defaultStyling() {
+    // rounds style
+    tutOverlay.style.alignItems = "center";
+    roundOne.style.zIndex = "unset";
+
+    // imported div.
+    let workshopOne = sendWorkshop();
+    // workshop style.
+    workshopOne.style.zIndex = "unset";
+
+    let iconOne = sendInfoIcon();
+    iconOne.style.zIndex = "unset";
+
+    let roundOneX = sendRoundX();
+    roundOneX.style.color = "white";
+
+    if (roundOne.classList.contains("tutorial-highlight")) {
+        roundOne.classList.remove("tutorial-highlight");
+    }
+    if (workshopOne.classList.contains("tutorial-highlight")) {
+        workshopOne.classList.remove("tutorial-highlight");
+    }
+}
+
+function firstStep() {
+    defaultStyling() // resets previous styling.
+
+    let iconOne = sendInfoIcon();
+
+    iconOne.style.zIndex = "1001";
+}
+
+// each step with styling.
 function secondStep() {
-    workshopOne = sendWorkshop();
-    // rounds
+    defaultStyling() // resets previous styling.
+
+    // rounds style
+    tutOverlay.style.alignItems = "end";
+    roundOne.style.zIndex = "1001";
+    
+    roundOne.classList.add("tutorial-highlight")
+}
+
+function thirdStep() {
+    defaultStyling() // resets previous styling.
+
+    // imported div.
+    let workshopOne = sendWorkshop();
+
+    // rounds style.
     tutOverlay.style.alignItems = "end";
     roundOne.style.zIndex = "1001";
 
+    // workshop style.
     workshopOne.style.zIndex = "1001";
+    workshopOne.classList.add("tutorial-highlight")
+}
 
+function fourthStep() {
+    defaultStyling() // resets previous styling.
+
+    let roundOneX = sendRoundX();
+
+    roundOneX.style.color = "red";
 }
