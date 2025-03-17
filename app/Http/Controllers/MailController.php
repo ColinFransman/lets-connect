efebcdb779d29a3c0467a02d7c7c4bcc2890412b
ColinFransman <colinfransman1@gmail.com>
2025-03-17 08:58:15 +0100

    feat: mail template now uses workshops from DB

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SendMail;
@@ -32,7 +33,7 @@ class MailController extends Controller
 
             // Redirect naar een succespagina na het versturen van de e-mail
             return redirect('/success')->with(['status' => 'success', 'title' => 'Bedankt voor je aanmelding', 'message' => 'Je planning is succesvol opgeslagen. Je ontvangt zo snel mogelijk een mail ter bevestiging van je gekozen planning. Je kan dit scherm nu sluiten.']);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             // Als er iets misgaat, toon een foutmelding
             return redirect('/dashboard')->with(['status' => 'error', 'message' => 'Er is iets misgegaan :\'(']);
         }
