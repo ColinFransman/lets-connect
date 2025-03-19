c37d28f6f900542199bf2a38f18ff4eba5877cc0
ColinFransman <colinfransman1@gmail.com>
2025-03-19 08:47:44 +0100

    feat: swapping workshops updates form

--- public/js/dragAndDrop.js
+++ public/js/dragAndDrop.js
@@ -44,6 +44,22 @@ function drop(ev) {
         addCloseButton(draggedElement, targetRound);
         workshopsInRounds.add(draggedElement.id);
         checkWorkshopsInRounds();
+    } else {
+        oldWorkshop = (targetRound.firstChild); //workshopelement waarmee je wil swappen, draggedElement is het te swappen workshopelement
+        oldRound = draggedElement.parentNode; //ronde-element waar de workshop terecht komt waarmee je wil swappen, targetRound is het element waar de te swappen workshop terecht komt
+        if (targetRound.firstChild.id != "workshop0" && targetRound.firstChild.id != "workshop1" && targetRound.firstChild.id != "workshop2") { //disable swap-acties met theaterworkshops 
+            let draggedTitle = draggedElement.querySelector(".title"); //get title element van de te swappen workshop
+            let oldTitle = oldWorkshop.querySelector(".title"); //get title element van de workshop waarmee je wil swappen
+            let draggedXpath = `//input[@value="` + oldTitle.getAttribute('workshop') + `"]`;
+            let draggedMatchingElement = document.evaluate(draggedXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue; //get hidden input van bovenstaand element
+            let oldXpath = `//input[@value="` + draggedTitle.getAttribute('workshop') + `"]`;
+            let oldMatchingElement = document.evaluate(oldXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            console.log("voor het updaten:", draggedMatchingElement); //log oude input van de te swappen workshop
+            draggedMatchingElement.value = draggedTitle.getAttribute('workshop'); //set oude input van de te swappen workshop naar value van de workshop waarmee je wil swappen
+            oldMatchingElement.value = oldTitle.getAttribute('workshop'); 
+            targetRound.replaceChild(draggedElement, oldWorkshop);
+            oldRound.appendChild(oldWorkshop);
+        }
     }
     updateSaveButton();
 }
