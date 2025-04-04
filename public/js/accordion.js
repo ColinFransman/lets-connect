7b1c9eb94110c7063ad562770d8718b28e3a001f
ColinFransman <colinfransman1@gmail.com>
2025-04-04 09:58:55 +0200

    feat: workshops on wdashboard are no longer collapsed by default

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -1,24 +1,22 @@
 function toggleAccordion(button) {
     let content = button.nextElementSibling;
     let isOpen = content.style.display === "block";
-
-    document.querySelectorAll('.accordion-content').forEach(div => div.style.display = 'none');
-    document.querySelectorAll('.accordion-btn span:last-child').forEach(span => span.textContent = '+');
-    document.querySelectorAll('.accordion-btn').forEach(btn => {
-        btn.classList.remove('bg-orange-600', 'text-white');
-        btn.classList.add('bg-blue-600', 'text-white');
-    });
+    console.log(button)
+    button.parentNode.querySelector('.accordion-content').style.display = 'none';
+    button.parentNode.querySelector('.accordion-btn span:last-child').textContent = '+';
+    button.parentNode.querySelector('.accordion-btn').classList.remove('bg-orange-600');
+    button.parentNode.querySelector('.accordion-btn').classList.add('bg-blue-600');
 
     if (!isOpen) {
         content.style.display = "block";
         button.querySelector('span:last-child').textContent = '-';
         button.classList.remove('bg-blue-600');
-        button.classList.add('bg-orange-600', 'text-white');
+        button.classList.add('bg-orange-600');
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.accordion-content').forEach(div => div.style.display = 'none');
-    document.querySelectorAll('.accordion-btn span:last-child').forEach(span => span.textContent = '+');
-    document.querySelectorAll('.accordion-btn').forEach(btn => btn.classList.add('bg-blue-600', 'text-white'));
+    document.querySelectorAll('.accordion-content').forEach(div => div.style.display = 'block');
+    document.querySelectorAll('.accordion-btn span:last-child').forEach(span => span.textContent = '-');
+    document.querySelectorAll('.accordion-btn').forEach(btn => btn.classList.add('bg-orange-600', 'text-white'));
 });
