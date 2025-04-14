9bb84087941afe5d4774c1b6a226afa2d9c396b8
ColinFransman <colinfransman1@gmail.com>
2025-04-14 12:43:48 +0200

    feat: added workshoplocation to email

--- resources/views/emailstemplate.blade.php
+++ resources/views/emailstemplate.blade.php
@@ -44,17 +44,23 @@
 
 <div class="container">
     <h1>Beste {{ auth()->user()->name }},</h1>
-    <p>Bedankt voor je aanmelding! Je planning is succesvol opgeslagen. Hier zijn de details:</p>
+    <p>
+       Bedankt voor je aanmelding! 
+       Leuk dat jij je hebt opgegeven voor netwerkevenement Let's Connect.
+       Je wordt om 12:30 op het Sweelinckplein verwacht voor de centrale opening.
+       Hier zijn de details van de workshops die jij hebt gekozen:
+    </p>
 
     <table>
         <tr>
             <th>Datum</th>
             <th>Tijd</th>
             <th>Workshop</th>
+            <th>Lokaal</th>
         </tr>
         <tr>
             <td>27-05-2025</td>
-            <td>13:00-13:45</td>
+            <td>13:00 - 13:45</td>
             <td>
                 @php
                     $results = 
@@ -71,10 +77,26 @@
                     }
                 @endphp
             </td>
+            <td>
+                @php
+                    $results = 
+                    DB::table('workshops AS ws')
+                    ->join('workshop_moments AS wm', 'ws.id', '=', 'wm.workshop_id')
+                    ->join('bookings AS bk', 'wm.id', '=', 'bk.wm_id')
+                    ->select('ws.location')
+                    ->where('bk.student_id', '=', auth()->user()->id)
+                    ->where('wm.moment_id', '=', '1')
+                    ->get()->toArray();
+
+                    foreach ($results as $result) {
+                        echo $result->location;
+                    }
+                @endphp
+            </td>
         </tr>
         <tr>
             <td>27-05-2025</td>
-            <td>13:45-14:30</td>
+            <td>13:45 - 14:30</td>
             <td>
                 @php
                     $results = 
@@ -91,10 +113,26 @@
                     }
                 @endphp
             </td>
+            <td>
+                @php
+                    $results = 
+                    DB::table('workshops AS ws')
+                    ->join('workshop_moments AS wm', 'ws.id', '=', 'wm.workshop_id')
+                    ->join('bookings AS bk', 'wm.id', '=', 'bk.wm_id')
+                    ->select('ws.location')
+                    ->where('bk.student_id', '=', auth()->user()->id)
+                    ->where('wm.moment_id', '=', '2')
+                    ->get()->toArray();
+
+                    foreach ($results as $result) {
+                        echo $result->location;
+                    }
+                @endphp
+            </td>
         </tr>
         <tr>
             <td>27-05-2025</td>
-            <td>15:00-15:45</td>
+            <td>15:00 - 15:45</td>
             <td>
                 @php
                     $results = 
@@ -108,6 +146,22 @@
 
                     foreach ($results as $result) {
                         echo $result->name;
+                    }
+                @endphp
+            </td>
+            <td>
+                @php
+                    $results = 
+                    DB::table('workshops AS ws')
+                    ->join('workshop_moments AS wm', 'ws.id', '=', 'wm.workshop_id')
+                    ->join('bookings AS bk', 'wm.id', '=', 'bk.wm_id')
+                    ->select('ws.location')
+                    ->where('bk.student_id', '=', auth()->user()->id)
+                    ->where('wm.moment_id', '=', '3')
+                    ->get()->toArray();
+
+                    foreach ($results as $result) {
+                        echo $result->location;
                     }
                 @endphp
             </td>
