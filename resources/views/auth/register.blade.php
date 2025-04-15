957721f664183a88949fe3a404f900f00a5316a2
ColinFransman <colinfransman1@gmail.com>
2025-04-15 10:56:44 +0200

    chore: removed unnecessary div

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -49,10 +49,9 @@
                             <option value="opleiding13">Creative Development</option>
                         </select>
                     </div>
-                        <select id="klas" name="klas">
-                            <option value="">Kies een klas</option>
-                        </select>
-                    </div>
+                    <select id="klas" name="klas">
+                        <option value="">Kies een klas</option>
+                    </select>
                     <x-input-error :messages="$errors->get('klas')" class="mt-2" />
                 </div>
 
