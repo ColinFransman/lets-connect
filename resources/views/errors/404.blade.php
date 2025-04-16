378038fd0cd44691c30a879e146b2fef4c799287
idontknow83 <manoncristel37@gmail.com>
2025-04-16 17:39:48 +0200

    fixed: errorpages bug

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,7 +2,7 @@
 
 @section('message')
     <div class="message-box">
-        <pre>Je hebt geen toegang tot deze pagina</pre>
+        <pre>De pagina is niet gevonden</pre>
         <div class="countdown">
             <pre>Je wordt over&nbsp;</pre>
             <pre id="timer">5</pre>
@@ -10,25 +10,4 @@
         </div>
     </div>
     <img class="background-container" src="{{ asset('/images/24213_SAVETHEDATE_LETS_CONNECT_01.jpg') }}">
-
-    <script>
-        let countdown = 5;  
-        const timerElement = document.getElementById('timer');
-
-        const interval = setInterval(() => {
-            countdown--;
-            timerElement.textContent = countdown;
-
-            if (countdown === 0) {
-                clearInterval(interval);
-
-                setTimeout(() => {
-                    window.location.href = "{{ url('/dashboard') }}";  
-                }, 1000);
-            }
-        }, 1000);
-    </script>
-
-</body>
-</html>
 @endsection
